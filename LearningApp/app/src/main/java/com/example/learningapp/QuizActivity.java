630076e9f0fe092702c56904cdff0ca41439155e
unknown <rfasih14@gmail.com>
2022-06-12 20:18:04 +0500

    changes

--- LearningApp/app/src/main/java/com/example/learningapp/QuizActivity.java
+++ LearningApp/app/src/main/java/com/example/learningapp/QuizActivity.java
@@ -6,9 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 public class QuizActivity extends AppCompatActivity {
     String[] key= {"b","c","a","d","a","a","d"};
+    Button submitBtn;
+    RadioGroup g1,g2,g3,g4,g5,g6,g7;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +24,12 @@ public class QuizActivity extends AppCompatActivity {
                 new ColorDrawable(Color.parseColor("#F50057"));
         assert actionBar != null;
         actionBar.setBackgroundDrawable(colorDrawable);
+        submitBtn = findViewById(R.id.submitQuiz);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
