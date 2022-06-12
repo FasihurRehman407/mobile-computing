b7561d2c9b277f3fba6e1b456dcc6e473b350d31
unknown <rfasih14@gmail.com>
2022-06-12 16:26:15 +0500

    changes

---
+++ LearningApp/app/src/main/java/com/example/learningapp/QuizActivity.java
@@ -0,0 +1,23 @@
+package com.example.learningapp;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+        ColorDrawable colorDrawable =
+                new ColorDrawable(Color.parseColor("#F50057"));
+        assert actionBar != null;
+        actionBar.setBackgroundDrawable(colorDrawable);
+    }
+}
