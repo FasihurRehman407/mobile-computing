38077f0a7b04ed855dccbfb5919068072152b080
unknown <rfasih14@gmail.com>
2022-09-06 23:18:44 +0500

    changes

--- LearningApp/app/src/main/java/com/example/learningapp/LessonActivity.java
+++ LearningApp/app/src/main/java/com/example/learningapp/LessonActivity.java
@@ -3,6 +3,8 @@ package com.example.learningapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LessonActivity extends AppCompatActivity {
 
@@ -10,5 +12,39 @@ public class LessonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson);
+        findViewById(R.id.buttonA).setOnClickListener((View.OnClickListener) this);
+        findViewById(R.id.buttonB).setOnClickListener((View.OnClickListener) this);
+        findViewById(R.id.buttonC).setOnClickListener((View.OnClickListener) this);
+        findViewById(R.id.buttonB).setOnClickListener((View.OnClickListener) this);
+        findViewById(R.id.buttonB).setOnClickListener((View.OnClickListener) this);
+        findViewById(R.id.buttonB).setOnClickListener((View.OnClickListener) this);
+        findViewById(R.id.buttonB).setOnClickListener((View.OnClickListener) this);
+        findViewById(R.id.buttonB).setOnClickListener((View.OnClickListener) this);
+        findViewById(R.id.buttonB).setOnClickListener((View.OnClickListener) this);
+        findViewById(R.id.buttonB).setOnClickListener((View.OnClickListener) this);
+        findViewById(R.id.buttonB).setOnClickListener((View.OnClickListener) this);
+        findViewById(R.id.buttonB).setOnClickListener((View.OnClickListener) this);
+    }
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            // cases applied over different buttons
+            case R.id.button:
+
+                // Toast message appears when button pressed
+                Toast.makeText(this, "button1 pressed", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.button2:
+
+                Toast.makeText(this, "button2 pressed", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.button3:
+
+                Toast.makeText(this, "button3 pressed", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
