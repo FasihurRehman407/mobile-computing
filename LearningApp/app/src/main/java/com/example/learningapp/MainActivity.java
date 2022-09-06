430b2b34f1cd89680470f401fd26ab0a82128aa1
unknown <rfasih14@gmail.com>
2022-09-06 23:02:29 +0500

    changes

--- LearningApp/app/src/main/java/com/example/learningapp/MainActivity.java
+++ LearningApp/app/src/main/java/com/example/learningapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.learningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
         lessonBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                Intent intent = new Intent(MainActivity.this,LessonActivity.class);
+                startActivity(intent);
             }
         });
 
