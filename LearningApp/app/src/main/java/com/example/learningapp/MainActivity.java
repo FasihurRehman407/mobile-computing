82621ca32bae36f0bf8702334ccabbef7a8012b1
unknown <rfasih14@gmail.com>
2022-09-06 22:58:27 +0500

    changes

--- LearningApp/app/src/main/java/com/example/learningapp/MainActivity.java
+++ LearningApp/app/src/main/java/com/example/learningapp/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.learningapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button lessonBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lessonBtn=findViewById(R.id.lessonBtn);
+        lessonBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
     }
 }
