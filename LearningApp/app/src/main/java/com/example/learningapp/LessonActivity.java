4f21ba050cd4f1c0796837a18fa1b7c7e4558fbe
unknown <rfasih14@gmail.com>
2022-09-06 23:39:29 +0500

    changes

--- LearningApp/app/src/main/java/com/example/learningapp/LessonActivity.java
+++ LearningApp/app/src/main/java/com/example/learningapp/LessonActivity.java
@@ -2,48 +2,56 @@ package com.example.learningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LessonActivity extends AppCompatActivity {
+public class LessonActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson);
-        findViewById(R.id.buttonA).setOnClickListener((View.OnClickListener) this);
-        findViewById(R.id.buttonB).setOnClickListener((View.OnClickListener) this);
-        findViewById(R.id.buttonC).setOnClickListener((View.OnClickListener) this);
-        findViewById(R.id.buttonB).setOnClickListener((View.OnClickListener) this);
-        findViewById(R.id.buttonB).setOnClickListener((View.OnClickListener) this);
-        findViewById(R.id.buttonB).setOnClickListener((View.OnClickListener) this);
-        findViewById(R.id.buttonB).setOnClickListener((View.OnClickListener) this);
-        findViewById(R.id.buttonB).setOnClickListener((View.OnClickListener) this);
-        findViewById(R.id.buttonB).setOnClickListener((View.OnClickListener) this);
-        findViewById(R.id.buttonB).setOnClickListener((View.OnClickListener) this);
-        findViewById(R.id.buttonB).setOnClickListener((View.OnClickListener) this);
-        findViewById(R.id.buttonB).setOnClickListener((View.OnClickListener) this);
+        findViewById(R.id.buttonA).setOnClickListener(this);
+        findViewById(R.id.buttonB).setOnClickListener(this);
+        findViewById(R.id.buttonC).setOnClickListener(this);
+        findViewById(R.id.buttonD).setOnClickListener(this);
+        findViewById(R.id.buttonE).setOnClickListener(this);
+        findViewById(R.id.buttonF).setOnClickListener( this);
+        findViewById(R.id.buttonG).setOnClickListener(this);
+        findViewById(R.id.buttonH).setOnClickListener(this);
+        findViewById(R.id.buttonI).setOnClickListener(this);
+        findViewById(R.id.buttonJ).setOnClickListener(this);
+        findViewById(R.id.buttonK).setOnClickListener(this);
+        findViewById(R.id.buttonL).setOnClickListener( this);
+        findViewById(R.id.buttonM).setOnClickListener( this);
+        findViewById(R.id.buttonN).setOnClickListener(this);
+        findViewById(R.id.buttonO).setOnClickListener( this);
+        findViewById(R.id.buttonP).setOnClickListener(this);
+        findViewById(R.id.buttonQ).setOnClickListener( this);
+        findViewById(R.id.buttonR).setOnClickListener( this);
+        findViewById(R.id.buttonS).setOnClickListener(this);
+        findViewById(R.id.buttonT).setOnClickListener(this);
+        findViewById(R.id.buttonU).setOnClickListener(this);
+        findViewById(R.id.buttonV).setOnClickListener(this);
+        findViewById(R.id.buttonW).setOnClickListener(this);
+        findViewById(R.id.buttonX).setOnClickListener(this);
+        findViewById(R.id.buttonY).setOnClickListener(this);
+        findViewById(R.id.buttonZ).setOnClickListener(this);
     }
+    @Override
     public void onClick(View v) {
 
         switch (v.getId()){
 
-            // cases applied over different buttons
-            case R.id.button:
-
-                // Toast message appears when button pressed
-                Toast.makeText(this, "button1 pressed", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.button2:
-
-                Toast.makeText(this, "button2 pressed", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.button3:
-
-                Toast.makeText(this, "button3 pressed", Toast.LENGTH_SHORT).show();
+            case R.id.buttonA:
+                Intent i = new Intent(LessonActivity.this , LessonInfo.class);
+                String message = "A for apple";
+                i.putExtra("Aa", message);
+                startActivity(i);
                 break;
         }
     }
--- LearningApp/app/src/main/java/com/example/learningapp/LessonInfo.java
+++ LearningApp/app/src/main/java/com/example/learningapp/LessonInfo.java
@@ -2,7 +2,9 @@ package com.example.learningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LessonInfo extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class LessonInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson_info);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("Aa");
+        TextView textView = findViewById(R.id.textView3);
+        textView.setText(message);
     }
 }
