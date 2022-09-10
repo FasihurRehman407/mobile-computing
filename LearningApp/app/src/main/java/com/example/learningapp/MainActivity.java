6aaf880fc95c05596110ac82118fd442764e0596
unknown <rfasih14@gmail.com>
2022-09-10 22:20:25 +0500

    changes

--- LearningApp/app/src/main/java/com/example/learningapp/MainActivity.java
+++ LearningApp/app/src/main/java/com/example/learningapp/MainActivity.java
@@ -6,12 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button lessonBtn;
+    Button lessonBtn , reposBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +24,15 @@ public class MainActivity extends AppCompatActivity {
         assert actionBar != null;
         actionBar.setBackgroundDrawable(colorDrawable);
         lessonBtn=findViewById(R.id.lessonBtn);
+        reposBtn=findViewById(R.id.reposBtn);
+        reposBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://www.github.com/FasihurRrehman407");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
         lessonBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
